ecefe1173474bc5c7f1c53e3325707935657cc94
Aleksandar <al.nikolic.za@gmail.com>
2025-12-13 06:08:09 +0100

    ✨ feat: expose routes count grouped by HTTP method at /dev/routes

--- controllers/devController.js
+++ controllers/devController.js
@@ -6,6 +6,21 @@ import { generateFileTreeString, countNodes } from '../services/devService.js';
 
 export const getAllRoutesListController = (req, res) => {
   const routes = listRoutes(req.app, { logger: false });
+
+  const routesAdditionalInfo = {
+    total: routes.length,
+    byMethod: {
+      GET: routes.filter((r) => r.method.includes('GET')).length,
+      POST: routes.filter((r) => r.method.includes('POST')).length,
+      PUT: routes.filter((r) => r.method.includes('PUT')).length,
+      PATCH: routes.filter((r) => r.method.includes('PATCH')).length,
+      DELETE: routes.filter((r) => r.method.includes('DELETE')).length,
+    },
+  };
+
+  routes.unshift({
+    ...routesAdditionalInfo,
+  });
   res.status(200).json(routes);
 };
 
